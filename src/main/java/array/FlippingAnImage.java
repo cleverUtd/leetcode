8e73f158ff8035f160adef127c48577b3d8e2819
zclau <349856491@qq.com>
2019-06-04 16:40:41 +0800

    formatting code

--- src/main/java/array/FlippingAnImage.java
+++ src/main/java/array/FlippingAnImage.java
@@ -3,10 +3,11 @@ package array;
 public class FlippingAnImage {
 
 	public static int[][] flipAndInvertImage(int[][] A) {
-		int i,j,temp;
+		int i, j, temp;
 		for (int[] ints : A) {
-			i=0;j=ints.length-1;
-			while (i<j) {
+			i = 0;
+			j = ints.length - 1;
+			while (i < j) {
 				ints[i] = ints[i] == 1 ? 0 : 1;
 				ints[j] = ints[j] == 1 ? 0 : 1;
 				temp = ints[i];
@@ -39,7 +40,7 @@ public class FlippingAnImage {
 	}
 
 	public static void main(String[] args) {
-		print(flipAndInvertImage(new int[][]{{1},{1},{0}}));
-		print(flipAndInvertImage(new int[][]{{1,1,0,0},{1,0,0,1},{0,1,1,1},{1,0,1,0}}));
+		print(flipAndInvertImage(new int[][]{{1}, {1}, {0}}));
+		print(flipAndInvertImage(new int[][]{{1, 1, 0, 0}, {1, 0, 0, 1}, {0, 1, 1, 1}, {1, 0, 1, 0}}));
 	}
 }
